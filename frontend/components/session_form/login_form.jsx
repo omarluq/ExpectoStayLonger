73c10eb06f5a572abf8acfced7f6b74099f6036e
omarluq <omaralanii@outlook.com>
2021-09-12 09:29:13 -0700

    update

--- frontend/components/session_form/login_form.jsx
+++ frontend/components/session_form/login_form.jsx
@@ -34,7 +34,7 @@ class LoginForm extends React.Component {
 
     render(){
         let errors;
-        if (this.props.errors){
+        if (this.props.errors.length > 0){
             errors = this.props.errors.map((error, i)=> <li className="errors" key={i}>{error}</li>)} else {
                 errors = <div></div>
             }
