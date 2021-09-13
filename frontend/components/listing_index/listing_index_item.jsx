46743135f9dad87ee62611ed4e587e78d4484351
omarluq <omaralanii@outlook.com>
2021-09-13 15:38:19 -0700

    update

--- frontend/components/listing_index/listing_index_item.jsx
+++ frontend/components/listing_index/listing_index_item.jsx
@@ -46,7 +46,6 @@ const ListingItem = (props) => {
           </p>
           <br />
           <address>
-            {props.listing.street_address}, <br />
             {props.listing.city}, {props.listing.country}
           </address>
           <br />
