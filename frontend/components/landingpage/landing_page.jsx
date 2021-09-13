079bc5a881bb28bbf8fac84b749ea8206a012066
omarluq <omaralanii@outlook.com>
2021-09-12 17:06:16 -0700

    update

--- frontend/components/landingpage/landing_page.jsx
+++ frontend/components/landingpage/landing_page.jsx
@@ -13,68 +13,71 @@ const LandingPage = (props) => {
         </div>
         <div className="featured">
           <div className="featured-icon">
-            <div className="sorting-hat"></div>
+            
             <a
-              className="footerlink"
+              className="featuredlink"
               href="https://www.wizardingworld.com/news/discover-your-hogwarts-house-on-wizarding-world"
               target="_blank"
-            >
-              Discover your Hogwarts house
+            > <div className="sorting-hat"></div>
+              <p>Discover your Hogwarts house</p>
             </a>
           </div>
 
           <div className="featured-icon">
-            <div className="room"></div>
+            
             <a
-              className="footerlink"
+              className="featuredlink"
               href="https://www.wizardingworld.com/features/everything-you-need-to-know-about-the-room-of-requirement"
               target="_blank"
-            >
-              Learn about the Room of Requirement{" "}
+            > <div className="room"></div>
+              <p>Learn about the Room of Requirement</p>
             </a>
           </div>
 
           <div className="featured-icon">
-            <div className="patronus"></div>
+            
             <a
-              className="footerlink"
+              className="featuredlink"
               href="https://www.wizardingworld.com/features/what-is-a-patronus"
               target="_blank"
-            >
-              Discover your Patronus{" "}
+            ><div className="patronus"></div>
+              <p>Discover your Patronus</p>
             </a>
           </div>
 
           <div className="featured-icon">
-            <div className="hallows"></div>
+            
             <a
-              className="footerlink"
+              className="featuredlink"
               href="https://www.wizardingworld.com/features/10-things-we-love-about-deathly-hallows"
               target="_blank"
-            >
-              Learn about the Deathly Hallows
+            > <div className="hallows"></div>
+              <p>Learn about the Deathly Hallows</p>
+              
             </a>
           </div>
 
           <div className="featured-icon">
-            <div className="ollivanders"></div>
+            
             <a
-              className="footerlink"
+              className="featuredlink"
               href="https://www.wizardingworld.com/news/discover-your-wand-on-pottermore"
               target="_blank"
-            >
-              Discover your Wand
+            > <div className="ollivanders"></div>
+              <p>Discover your Wand</p>
+              
             </a>
           </div>
 
           <div className="featured-icon">
-            <div className="broomstick"></div>
+            
             <a
-              className="footerlink"
+              className="featuredlink"
               href="https://www.wizardingworld.com/features/things-you-would-use-broomsticks-for-other-than-flying"
               target="_blank"
-            >
-              A Funny article about broomsticks
+            > <div className="broomstick"></div>
+              <p>A Funny article about broomsticks</p>
+              
             </a>
           </div>
         </div>
