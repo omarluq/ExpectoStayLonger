26f9079c9692ca69c1526c2e82dda62dca6a1d16
omarluq <omaralanii@outlook.com>
2021-09-15 09:13:38 -0700

    update

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -3,10 +3,12 @@ import React from "react";
 import { Provider } from "react-redux";
 import { HashRouter } from "react-router-dom";
 import App from "./app";
+import ScrollToTop from "./scroll_to_top";
 
 const Root = ({ store }) => (
   <Provider store={store}>
     <HashRouter>
+      < ScrollToTop />
       <App />
     </HashRouter>
   </Provider>
